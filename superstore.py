c11815590dacb5571657a695dfc52cb07be33ee5
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-03 18:05:05 -0500

    Create mode function

--- superstore.py
+++ superstore.py
@@ -1,16 +1,32 @@
-#from statistics import mean, mode, multimode
+from statistics import mean, mode, multimode
 #Varios analísis
+
+def mode_func(iterable):
+    elements = set(iterable)
+    dic = {}
+    for i in elements:
+        frequency = 0
+        for _ in iterable:
+            if i == _:
+                frequency += 1
+        dic[i] = frequency
+    return dic
+
+def analysis(data):
+    frequency = mode_func(data)
+    print(frequency)
 
 def extraction(file_name):
     with open(f"./files/{file_name}.txt", "r", encoding="utf-8") as f:
-        generator = (x.strip("\n") for x in f)
-    return generator
+        data = [x.strip("\n") for x in f]
+    return data
 
 def run():
     ship_mode = extraction("ship_mode")
-    for i in ship_mode:
-        print(i)
-
+    segment = extraction("segment")
+    city = extraction("city")
+    state = extraction("state")
+    analysis(ship_mode)
 
 if __name__=="__main__":
     run()
