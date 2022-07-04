88649c9d31f962876d39e53c4413a1e4d7342afe
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-04 12:34:03 -0500

    Improved my program with POO and somethings in decorator

--- superstore.py
+++ superstore.py
@@ -1,48 +1,56 @@
-#Varios analísis
+from fields import Fields
 
 def max_and_min(func):
-    def wrapper(*args, **kwargs):
-        results = func(*args, **kwargs)
-        max_freq = max([value for value in results.values()])
-        min_freq = min([value for value in results.values()])
+    #f = frequency
+    def wrapper(field):
+        results = func(field)
+        max_f = max([value for value in results.values()])
+        min_f = min([value for value in results.values()])
+        max_field, min_field = [], []
         for key, value in results.items():
-            if max_freq == value:
-                print(f"Max: {key} with: {max_freq}")
-            elif min_freq == value:
-                print(f"Min:{key} with: {min_freq}")
+            if max_f == value:
+                max_field.append(key)
+            elif min_f == value:
+                min_field.append(key)
+        for k in field.keys():
+            print(f"""
+Most requested {k.replace("_", " ")}: {max_field}, f: {max_f} order(s)
+Least requested {k.replace("_", " ")}: {min_field}, f: {min_f} order(s)""")
     return wrapper
 
-
 def mode(iterable):
-    elements = set(iterable)
+    elements = set()
+    for v in iterable.values(): 
+        for _ in v:
+            elements.add(_)
     dic_frequencies = {}
-    for i in elements:
+    for e in elements:
         frequency = 0
-        for _ in iterable:
-            if i == _:
-                frequency += 1
-        dic_frequencies[i] = frequency
+        for v in iterable.values():
+            for _ in v:
+                if e == _:
+                    frequency += 1
+        dic_frequencies[e] = frequency
     return dic_frequencies
 
 @max_and_min
-def analysis(data):
+def analysis(data): #data = {"ship_mode": [Standard Class, Same Day,... ]}
     frequency = mode(data)
     return frequency
 
 def extraction(file_name):
+    data = {}
     with open(f"./files/{file_name}.txt", "r", encoding="utf-8") as f:
-        data = [x.strip("\n") for x in f]
+        value = [x.strip("\n") for x in f]
+        data [file_name] = value
     return data
 
-def run():
-    ship_mode = extraction("ship_mode")
-    segment = extraction("segment")
-    city = extraction("city")
-    state = extraction("state")
-    analysis(ship_mode)
-    analysis(segment)
-    analysis(city)
-    analysis(state)
-
 if __name__=="__main__":
-    run()
+    field_1 = Fields("ship_mode")
+    field_2 = Fields("segment")
+    field_3 = Fields("city")
+    field_4 = Fields("state")
+    analysis(extraction(field_1.name))
+    analysis(extraction(field_2.name))
+    analysis(extraction(field_3.name))
+    analysis(extraction(field_4.name))
