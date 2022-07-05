248278f72dcf89ec293095a3ee5af2c75a7e7562
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-05 11:35:28 -0500

    Added a mode function to find insights

--- superstore/claims.py
+++ superstore/claims.py
@@ -1,17 +1,38 @@
 
+
+def report():
+    def wrapper():
+        pass
+    return wrapper
+
+def mode(iterable):
+    elements = set()
+    for _ in iterable:   
+        elements.add(_)
+    dic_frequencies = {}
+    for e in elements:
+        frequency = 0
+        for _ in iterable:
+            if e == _:
+                frequency += 1
+        dic_frequencies[e] = frequency
+    return dic_frequencies
+
 def check_claims(data_base):
-    #Who are the claimers? Here we check it.
+    #Who are the claimers? In which city/state/region there more claims? Here we check it.
     #On id_claim.txt are all orders_id that were returned by a claim.
-    claimers = []
     order_id = []
+    claimers, city = [], []
+    state, region = [], []
     with open("./../files/id_claim.txt", "r", encoding="utf-8") as f:
         for i in f:
             order_id.append(i.strip("\n"))
     for dict in data_base:
         for _ in order_id:
             if dict["id"] == _:
-                claimers.append(dict["name"])
-    print(len(claimers))
+                claimers.append(dict["name"]), city.append(dict["city"])
+                state.append(dict["state"]), region.append(dict["region"])
+    return mode(claimers), mode(city), mode(state), mode(region)    
 
 def structure(data_wo_structure):  #wo = without
     #We save all data in a dictionary (database), but first you need to remove ",".
@@ -52,7 +73,8 @@ def extraction():
 def run():
     #Build a data base for your respective analysis.
     data_base = structure(cleaning(extraction()))
-    check_claims(data_base)
+    results = check_claims(data_base)
+    print(results)
 
 if __name__=="__main__":
     run()
