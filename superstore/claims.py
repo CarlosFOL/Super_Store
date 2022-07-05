e828d7efcb47b3c9b1fe20e03b63200afc66f0b0
Carlos Francisco Orejuela Lluncor <carlos_francisco_@hotmail.com>
2022-07-05 10:44:23 -0500

    Identify which customers have claimed in your order

--- superstore/claims.py
+++ superstore/claims.py
@@ -1,38 +1,50 @@
-from typing import List, Dict
 
-def data_base(data_wo_structure:List[str]) -> List[Dict[str:str]]:  #wo = without
-    fields:List[str] = [
+def check_claims(data_base):
+    claimers = []
+    order_id = []
+    with open("./../files/id_claim.txt", "r", encoding="utf-8") as f:
+        for i in f:
+            order_id.append(i.strip("\n"))
+    for dict in data_base:
+        for _ in order_id:
+            if dict["id"] == _:
+                claimers.append(dict["name"])
+    print(len(claimers))
+
+def structure(data_wo_structure):  #wo = without
+    fields = [
         "id","name", "city",
         "state", "region"
     ]
-    db = []
-    dic = {}
+    data_base = []
     value = []
-    for element in data_wo_structure:
+    for element in data_wo_structure: # element = CA-2016-152156,Claire Gute,Henderson,Kentucky,South,
         counter = 0
+        dic = {}
         for character in element:
             if character != ",":
-                value.append(character)
+                 value.append(character)
             else:
                 dic[fields[counter]] = "".join(value)
-                value.clear()
                 counter += 1
-        db.append(dic)
-    return db
+                value.clear()
+        data_base.append(dic)
+    return data_base
 
-def cleaning(data:List[str]) -> List[str]:
+def cleaning(data):
     for element in data:
         if element == "":
             data.remove(element)
     return data
 
-def extraction() -> List[str]:
+def extraction():
     with open("./../files/customers.txt", "r", encoding="utf-8") as f:
-        data:List[str] = [d.strip("\n") for d in f]
+        data = [d.strip("\n") for d in f]
     return data
 
 def run():
-    data_base(cleaning(extraction()))
-    
+    data_base = structure(cleaning(extraction()))
+    check_claims(data_base)
+
 if __name__=="__main__":
     run()
